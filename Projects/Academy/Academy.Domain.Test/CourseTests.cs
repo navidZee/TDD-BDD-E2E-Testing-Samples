9f1e69f87d0ce494344440f398ad26c13deea83c
Navid Zare <55227087+navidZee@users.noreply.github.com>
2021-12-22 21:48:51 +0330

    Refactor test cases

--- Projects/Academy/Academy.Domain.Test/CourseTests.cs
+++ Projects/Academy/Academy.Domain.Test/CourseTests.cs
@@ -21,28 +21,12 @@ namespace Academy.Domain.Test
             Assert.Equal(isOnline, course.IsOnline);
             Assert.Equal(tuition, course.Tuition);
         }
-        
-        [Fact]
-        public void Constructor_ShouldThrowException_When_NameIsNotProvided() // Happy path
+
+        [Theory]
+        [InlineData(1, "", true, 600)]
+        [InlineData(1, "TDD & BDD", true, 0)]
+        public void Constructor_ShouldThrowException(int id, string name, bool isOnline, double tuition)
         {
-            const int id = 1;
-            const string name = "";
-            const bool isOnline = true;
-            const double tuition = 600;
-
-            void Course() => new Course(id, name, isOnline, tuition);
-
-            Assert.Throws<Exception>(Course);
-        } 
-        
-        [Fact]
-        public void Constructor_ShouldThrowException_When_TuitionIsNotProvided() // Happy path
-        {
-            const int id = 1;
-            const string name = "TDD & BDD";
-            const bool isOnline = true;
-            const double tuition = 0;
-
             void Course() => new Course(id, name, isOnline, tuition);
 
             Assert.Throws<Exception>(Course);
